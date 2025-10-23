0990634007a341ca163c81e6906a7f8e97a9a90a
HerickRodrigues <herickcj@hotmail.com>
2025-10-23 18:58:52 -0300

    0. Uso da Nomenclatura

---
+++ 0x00/nomenclatura/Name.java
@@ -0,0 +1,27 @@
+public class Name {
+    // Variables with self-explanatory names
+    public Date modifiedDate;
+    public List<Testcase> findAllTestcasesByUser(Name.User user) {};
+
+    // List declaration
+    public List<Account> AccountList;
+
+    // No prefixes in parameters
+    public void deleteUser(Name.User user) {};
+
+    // Clear and descriptive variable names
+    public URL url;
+
+    // Classes with proper names and methods with verbs
+    public class TableRepresenter {};
+    public void saveCar() {};
+
+    // Consistent verb usage for similar operations
+    public void saveUser() {};
+    public void saveAccount() {};
+    public void saveContract() {};
+
+    // Constants with clear names
+    public final int DAYS_IN_YEAR = 360;
+    public final int HOURS_IN_WEEK = 168;
+}
